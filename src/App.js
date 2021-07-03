0c4a836c42ed378c092651d02ed6630dfec9ae65
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-03 14:49:52 +0100

    added search form div to layout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
   return (
     <div>
       <div className="border header">Header</div>
-      <div className="row main g-0">
+      {/* <div className="row main g-0">
         <div className="border col-sm-12 col-md-4">Country Card</div>
         <div className="border col-sm-12 col-md-8">
           <div className="border weather">Weather Card</div>
           <div className="border health">Health Card</div>
         </div>
+      </div> */}
+
+      <div className="border position-absolute top-50 start-50 translate-middle">
+        Search Form
       </div>
     </div>
   );
