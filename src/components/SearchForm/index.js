323e7e376aa8b7c5d81003406a32b483bf0a5513
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-03 15:19:27 +0100

    added styled search form

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,25 @@
+const SearchForm = (props) => {
+  return (
+    <div className="position-absolute top-50 start-50 translate-middle p-5 search-container">
+      <form onSubmit={props.onSubmit}>
+        <h2 className="pb-3">{props.text}</h2>
+        <div className="input-group my-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder={props.placeholder}
+          />
+          <button
+            className="btn btn-outline-secondary search-btn"
+            type="submit"
+            id="search-btn"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
